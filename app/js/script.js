a5d18b9fa92264dcfc23b49b63892d0e2f5abff9
Rudranshu Datta <rdatta2912@gmail.com>
2021-01-10 13:05:12 -0500

    new files

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,27 @@
-console.log('hello there');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+    body.classList.remove('noscroll');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        }); 
+        
+        
+    } else {
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+        
+    }
+});
